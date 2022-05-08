211ea482e836cbd1f460750b7c2f54b094cc45d8
migue <migue@LAPTOP-FKTUUEN1>
2022-05-07 22:00:22 -0600

    Practica Programación defensiva

---
+++ src/programaciondefensiva/ProgramacionDefensiva.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programaciondefensiva;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Miguel
+ */
+public class ProgramacionDefensiva {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner scanner = new Scanner(System.in);
+        char opcion;
+        System.out.println("Ingrese un numero:");
+        int num1 = scanner.nextInt();
+        System.out.println("Ingrese un numero:");
+        int num2 = scanner.nextInt();
+        System.out.println("Ingrese la opcion de lo que desea: ");
+        System.out.println("A. Suma");
+        System.out.println("B. Resta");
+        System.out.println("C. Multiplicacion");
+        System.out.println("D. Division");
+        opcion = scanner.next().toUpperCase().charAt(0);
+        System.out.println("El resultado de la operación es:");
+        switch(opcion)
+        {
+            case 'A':
+                System.out.println(Ejercicio1.suma(num1, num2));
+                break;
+                
+            case 'B':
+                System.out.println(Ejercicio1.resta(num1, num2));
+                break;
+            case 'C':
+                System.out.println(Ejercicio1.multiplicacion(num1, num2));
+                break;
+            case 'D':
+                System.out.println(Ejercicio1.division(num1, num2));
+                break;
+            default:
+                System.out.println("La opción ingresada no es valida");
+                break;
+                
+        }
+
+Ejercicio2 jugar= new Ejercicio2();
+jugar.juego();
+        
+    }
+    
+}
